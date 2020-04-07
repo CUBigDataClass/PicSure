2341f6271bdbdd959d202bcb40070ecb2485de80
Vivian Tran <vitr2218@colorado.edu>
2020-04-06 20:12:32 -0600

    Cleaned up files

--- angular/projects/sharedlibrary/src/lib/hash.service.ts
+++ angular/projects/sharedlibrary/src/lib/hash.service.ts
@@ -4,7 +4,6 @@ import {sha512} from 'js-sha512';
 @Injectable()
 export class HashService {
     hash(content: string): string {
-        // TODO: Generate a SHA512 hash of the content 
         const imageHash = sha512(content);
         return imageHash;
     }
