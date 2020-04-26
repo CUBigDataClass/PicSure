823e48db3f00dfbcaed0dd880bb55e08b69ec18a
Joshua Franklin <joshuafranklin26@gmail.com>
2020-04-25 20:48:22 -0400

    Add redis host build argument and add allow from anywhere header

--- api/app.py
+++ api/app.py
@@ -2,43 +2,47 @@ import json
 
 from os import environ
 from redis import Redis
-from flask import Flask, request
+from flask import Flask, request, Response
 
 
 app = Flask(__name__)
-redis = Redis('database')
+redis = Redis(environ['REDIS_HOST'])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    response = Response('')
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
     try:
         data = json.loads(request.get_data().decode())
     except json.JSONDecodeError:
-        return '', 400
+        response.status_code = 400
+        return response
 
     if request.method == 'GET':
         # Check if the user failed to specify any required data.
         if 'hash' not in data:
-            return '', 400
+            response.status_code = 400
 
         # Check if the hash exists in the database.
         if redis.exists(data['hash']):
-            return '', 200
+            response.status_code = 200
 
-        return '', 204
+        response.status_code = 204
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         # Check if the user failed to specify any required data.
         if 'hash' not in data or 'password' not in data:
-            return '', 400
+            response.status_code = 400
 
         # Make sure the camera has a valid password. If this check fails, someone is trying to spoof a camera.
         if data['password'] != environ['CAMERA_PASSWORD']:
-            return '', 401
+            response.status_code = 401
 
         # Save the image hash to the database.
         redis.set(data['hash'], 1)
 
-        return '', 200
+        response.status_code = 200
 
-    return 'Unsupported method.'
+    return response
