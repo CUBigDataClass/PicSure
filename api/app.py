0529aff4a14fbf591bd57e36d029ead56bb3ac49
Joshua Franklin <joshuafranklin26@gmail.com>
2020-04-25 21:57:29 -0400

    Convert to Waitress server

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ import json
 from os import environ
 from redis import Redis
 from flask import Flask, request, Response
+from waitress import serve
 
 
 app = Flask(__name__)
@@ -14,35 +15,43 @@ def index():
     response = Response('')
     response.headers['Access-Control-Allow-Origin'] = '*'
 
-    try:
-        data = json.loads(request.get_data().decode())
-    except json.JSONDecodeError:
-        response.status_code = 400
-        return response
-
     if request.method == 'GET':
         # Check if the user failed to specify any required data.
-        if 'hash' not in data:
+        if 'hash' not in request.args:
             response.status_code = 400
 
         # Check if the hash exists in the database.
-        if redis.exists(data['hash']):
+        elif redis.exists(request.args['hash']):
             response.status_code = 200
 
-        response.status_code = 204
+        else:
+            response.status_code = 204
 
     elif request.method == 'POST':
+        try:
+            data = json.loads(request.get_data().decode())
+        except json.JSONDecodeError:
+            response.status_code = 400
+            return response
+
         # Check if the user failed to specify any required data.
         if 'hash' not in data or 'password' not in data:
             response.status_code = 400
 
         # Make sure the camera has a valid password. If this check fails, someone is trying to spoof a camera.
-        if data['password'] != environ['CAMERA_PASSWORD']:
+        elif data['password'] != environ['CAMERA_PASSWORD']:
             response.status_code = 401
 
-        # Save the image hash to the database.
-        redis.set(data['hash'], 1)
+        else:
+            # Save the image hash to the database.
+            redis.set(data['hash'], 1)
+            response.status_code = 200
 
-        response.status_code = 200
+    else:
+        response.status_code = 400
 
     return response
+
+
+if __name__ == '__main__':
+    serve(app, port=5000)
